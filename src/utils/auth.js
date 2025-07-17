9cddf2f11c428b6d427c913c1efea4ef68236b6c
jeongneung <jeongneung2052@gmail.com>
2025-07-17 13:55:30 +0900

    환경변수, PWA

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,15 +1,19 @@
 export const isLoggedIn = () => {
-  return localStorage.getItem('isLoggedIn') === 'true';
+  return localStorage.getItem("isLoggedIn") === "true";
 };
 
 export const login = (id, password) => {
-  if (id === `${import.meta.env.VITE_ID}` && password === `${import.meta.env.VITE_PW}`) {
-    localStorage.setItem('isLoggedIn', 'true');
+  console.log("asdf" + import.meta.env.VITE_ID, import.meta.env.VITE_PW);
+  if (
+    id === `${import.meta.env.VITE_ID}` &&
+    password === `${import.meta.env.VITE_PW}`
+  ) {
+    localStorage.setItem("isLoggedIn", "true");
     return true;
   }
   return false;
 };
 
 export const logout = () => {
-  localStorage.removeItem('isLoggedIn');
+  localStorage.removeItem("isLoggedIn");
 };
